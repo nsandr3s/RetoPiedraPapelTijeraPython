07363811650f9aaa44c776e9fe80c4644054dee0
nsandr3s <n.orrego@outlook.cl>
2024-08-05 16:13:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+# Importa la función 'jugar' del módulo 'juego'
 from juego import jugar
 
 def main():
+    # Llama a la función 'jugar' que se ha importado del módulo 'juego'
     jugar()
 
+# Verifica si este archivo es el principal (el que se está ejecutando directamente)
 if __name__ == "__main__":
+    # Llama a la función 'main' si el archivo es el principal
     main()
